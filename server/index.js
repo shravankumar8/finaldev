ebd712ee6df2301e31ab980d6cd9373536a7e0e0
shravankumar8 <kumashravan5@gmail.com>
2024-02-25 11:57:51 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
 
@@ -9,11 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
-
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
 
-app.listen(3000, () => console.log('Server runing on port 3000'));
+app.listen(3000, () => console.log("Server running on port 3000"));
