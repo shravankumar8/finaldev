35c723e843fac57fd96b4543827c2ea60f3c34df
shravankumar8 <kumashravan5@gmail.com>
2024-02-25 23:29:49 +0530

    hh

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v21.6.2/bin/
 
 cd finaldev
- git pull origin master
+sudo git pull origin main
+
  cd server
- pm2 kill
- pm2 start index.js
+/home/ubuntu/.nvm/versions/node/v21.6.2/bin/pm2 kill
+/home/ubuntu/.nvm/versions/node/v21.6.2/bin/pm2 start index.js
