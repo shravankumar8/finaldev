23b1b2e8430138650291d207a57c305d67c35922
shravankumar8 <kumashravan5@gmail.com>
2024-02-27 20:57:04 +0530

    done

--- server/index.js
+++ server/index.js
@@ -11,9 +11,11 @@ app.use(express.json());
 
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
-app.get("/",(req,res)=>{res.send(
-  "In a quaint village, there lived a kind-hearted blacksmith named Aiden. One day, a mysterious traveler gifted him a small, silver key. Intrigued, Aiden discovered that the key unlocked a hidden door in his workshop. Behind it lay a room filled with forgotten dreams, waiting to be forged into reality. Inspired, Aiden spent his days bringing those dreams to life, turning his humble village into a haven of magic and possibility."
-);})
+app.get("/", (req, res) => {
+  res.send(
+    "In a quaint village, there lived a kind-hearted blacksmith named Aiden. One day, a mysterious traveler gifted him a small, silver key. Intrigued, Aiden discovered that the key unlked a hidden door in his workshop. Behind it lay a room filled with forgotten dreams, waiting to be forged into reality. Inspired, Aiden spent his days bringing those dreams to life, turning his humble village into a haven of magic and possibility."
+  );
+});
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
 
